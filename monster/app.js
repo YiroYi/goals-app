1bf0b3d07dde1c113d585ca8cfc3f55148cfe20b
YiroYi <yiroyi@live.com>
2020-10-04 17:25:46 -0500

    Special attack button implemented

--- monster/app.js
+++ monster/app.js
@@ -1,5 +1,5 @@
 const getRandomValue = (min, max) => {
-  Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 
@@ -7,20 +7,38 @@ const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
-      mosterHealth: 100
+      monsterHealth: 100,
+      currentRound: 0
     };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + '%' }
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + '%' }
+    },
+    mayUseSpecialAttack() {
+      return  this.currentRound % 3 !== 0
+    }
   },
   methods: {
     attackMoster() {
+      this.currentRound ++;
       const attackValue = getRandomValue(5, 12)
-      this.mosterHealth -= attackValue;
+      this.monsterHealth -= attackValue;
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15)
       this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound ++;
+      const attackValue = getRandomValue(10, 25)
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
     }
-
   }
 })
 
