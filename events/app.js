11196b820770402c166116a2b33da71b3e3c70a0
YiroYi <yiroyi@live.com>
2020-10-04 11:43:27 -0500

    v-on:input(event variable)

--- events/app.js
+++ events/app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
   methods: {
+    setName(eventn, name) {
+      this.name = event.target.value + name
+    },
     addNumber(num) {
       this.counter = this.counter + num
     },
