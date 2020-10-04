27fc4b672a670fdc2a1c3b9d370ef073c22c5fff
YiroYi <yiroyi@live.com>
2020-10-04 14:32:58 -0500

    v-if directive

---
+++ conditionals/app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enterGoalValue: ''
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enterGoalValue);
+    }
+  }
+});
+
+app.mount('#user-goals');
